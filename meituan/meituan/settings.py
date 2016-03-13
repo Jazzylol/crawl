598ead929c9076b18acb957a912348aa987befa6
Jazzylol <506496088@qq.com>
2016-03-14 00:24:31 +0800

    da

---
+++ meituan/meituan/settings.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for meituan project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'meituan'
+
+SPIDER_MODULES = ['meituan.spiders']
+NEWSPIDER_MODULE = 'meituan.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENTS = [
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+	"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+	"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+	"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+	"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+	"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+]
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+# CONCURRENT_REQUESTS=32
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+# DOWNLOAD_DELAY=3
+# The download delay setting will honor only one of:
+# CONCURRENT_REQUESTS_PER_DOMAIN=16
+# CONCURRENT_REQUESTS_PER_IP=16
+
+# Disable cookies (enabled by default)
+# COOKIES_ENABLED=False
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED=False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	"Accept-Encoding": "gzip, deflate",
+	"Accept-Language": "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3",
+	"Connection": "keep-alive",
+	"Cookie": "uuid=4ec469314c24592c17d4.1457786900.1.0.1; oc=K-biRgRZ5w1i5DPhik_ANdx0dQf5qEKDnpGtgE10DmOaN6lqDPDGb2SjhoDfpABTuN7vGfc6Jf0s8Q9z00EmCQBqpaHYO_Gpch5_kOuKmRnZAtrTlN7q2xHhIT22KNbViH_IEC8QXpHcc0r-mBBslUw5I0ipRLwxfw6EnGMTTXk; ci=59; abt=1457786900.0%7CBCF; __utma=211559370.120142566.1457786838.1457786838.1457788290.2; __utmc=211559370; __utmz=211559370.1457786838.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=211559370.|1=city=cd=1; __mta=216856489.1457786837515.1457790956603.1457791296341.24; SID=fkg0imnrl0sv7h9n0injiddrg0; n=IfV514662996; m=506496088%40qq.com; lt=tlHsPgWiAZDP4SUStAOUcytLFR8AAAAA7wEAAEUU-Lu_spj2xLegwKzOuMJktYV6ehYF6Vkul5io8zCwe3Y6MC0f7urhV4uNkPYdtA; lsu=506496088%40qq.com; token2=9RIWX0hMHy_3plNB6niiKTvHHUcAAAAA7gEAAB6s5X7Lqm9WEvhTD4SQc51C7onRESugoQ-mthyqxCjDUfkZuNMqmvEAQaQkaICh1w; u=127889406; em=bnVsbA; om=bnVsbA; ls=1457786960; vipnotice_127889406=%7B%22status%22%3Atrue%2C%22growthValue%22%3A629%2C%22showLevel%22%3A2%2C%22noticeType%22%3A0%2C%22noticeValue%22%3A0%2C%22cityid%22%3A59%7D; tipssecurequestion=1; ppos=38.0207%2C114.562651; pposn=%E9%87%8D%E5%BA%86%E7%A7%A6%E5%A6%88%E7%81%AB%E9%94%85%EF%BC%88%E8%A3%95%E5%8D%8E%E5%BA%97%EF%BC%89; rvct=76; __utmb=211559370.7.9.1457792016730",
+	"Host": "cd.meituan.com",
+	"Referer": "http://cd.meituan.com/category/huoguo/all/page2?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp73gir",
+	"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",
+	"Content-Length": "0",
+}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    'meituan.middlewares.MyCustomSpiderMiddleware': 543,
+# }
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+# DOWNLOADER_MIDDLEWARES = {
+#    'meituan.middlewares.MyCustomDownloaderMiddleware': 543,
+# }
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#    'scrapy.telnet.TelnetConsole': None,
+# }
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# ITEM_PIPELINES = {
+#    'meituan.pipelines.SomePipeline': 300,
+# }
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# NOTE: AutoThrottle will honour the standard settings for concurrency and delay
+# AUTOTHROTTLE_ENABLED=True
+# The initial download delay
+# AUTOTHROTTLE_START_DELAY=5
+# The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY=60
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG=False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
