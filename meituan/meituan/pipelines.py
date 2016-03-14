17a48aeb331f000e168eb20e10e37280fab27d0a
zhangzhitao <zhangzhitao.sh@superjia.com>
2016-03-14 18:10:08 +0800

    正式项目

--- meituan/meituan/pipelines.py
+++ meituan/meituan/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
 
 
 class MeituanPipeline(object):
+    def __init__(self):
+        self.file = codecs.open("data.txt", encoding='utf-8', mode='wb')
+
+    # def process_item(self, item, spider):
+    #     line = json.dumps(dict(item)) + '\n'
+    #     self.file.write(line.decode("unicode_escape"))
+    #     return item
+
     def process_item(self, item, spider):
+        divstr = eval(eval(str(item))['divstr'])['data'].decode("unicode_escape").replace("\/","/")
+        print(divstr)
         return item
--- meituan/meituan/settings.py
+++ meituan/meituan/settings.py
@@ -13,7 +13,7 @@ BOT_NAME = 'meituan'
 
 SPIDER_MODULES = ['meituan.spiders']
 NEWSPIDER_MODULE = 'meituan.spiders'
-
+ITEM_PIPELINES = {'meituan.pipelines.MeituanPipeline': 1}
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENTS = [
 	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
