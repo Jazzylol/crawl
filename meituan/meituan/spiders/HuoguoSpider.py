2370815c13db98b1c5451e0b1a1ee109d7cef834
zhangzhitao <zhangzhitao.sh@superjia.com>
2016-03-14 19:22:05 +0800

    正式项目

--- meituan/meituan/spiders/HuoguoSpider.py
+++ meituan/meituan/spiders/HuoguoSpider.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import sys
 
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import urllib
@@ -12,11 +11,17 @@ from json import JSONDecoder
 import scrapy
 from .. import items
 
-#美团爬虫
+
+# 美团爬虫
 class HuoguoSpider(scrapy.Spider):
+    def __init__(self):
+        self.page = 1
+
     name = "huoguo"
     allowed_domain = ["cd.meituan.com"]
-    start_urls = ["http://cd.meituan.com/category/huoguo/all/page3?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp7aqrm"]
+    start_urls = [
+        "http://cd.meituan.com/category/huoguo/all/page{}?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp7aqrm".format(str(page))
+        for page in range(10)]
 
     def parse(self, response):
         hxs = Selector(response)
@@ -29,7 +34,8 @@ class HuoguoSpider(scrapy.Spider):
             "Connection": "keep-alive",
             "Cookie": "uuid=4ec469314c24592c17d4.1457786900.1.0.1; oc=K-biRgRZ5w1i5DPhik_ANdx0dQf5qEKDnpGtgE10DmOaN6lqDPDGb2SjhoDfpABTuN7vGfc6Jf0s8Q9z00EmCQBqpaHYO_Gpch5_kOuKmRnZAtrTlN7q2xHhIT22KNbViH_IEC8QXpHcc0r-mBBslUw5I0ipRLwxfw6EnGMTTXk; ci=59; abt=1457786900.0%7CBCF; __utma=211559370.120142566.1457786838.1457786838.1457788290.2; __utmc=211559370; __utmz=211559370.1457786838.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=211559370.|1=city=cd=1; __mta=216856489.1457786837515.1457790956603.1457791296341.24; SID=fkg0imnrl0sv7h9n0injiddrg0; n=IfV514662996; m=506496088%40qq.com; lt=tlHsPgWiAZDP4SUStAOUcytLFR8AAAAA7wEAAEUU-Lu_spj2xLegwKzOuMJktYV6ehYF6Vkul5io8zCwe3Y6MC0f7urhV4uNkPYdtA; lsu=506496088%40qq.com; token2=9RIWX0hMHy_3plNB6niiKTvHHUcAAAAA7gEAAB6s5X7Lqm9WEvhTD4SQc51C7onRESugoQ-mthyqxCjDUfkZuNMqmvEAQaQkaICh1w; u=127889406; em=bnVsbA; om=bnVsbA; ls=1457786960; vipnotice_127889406=%7B%22status%22%3Atrue%2C%22growthValue%22%3A629%2C%22showLevel%22%3A2%2C%22noticeType%22%3A0%2C%22noticeValue%22%3A0%2C%22cityid%22%3A59%7D; tipssecurequestion=1; ppos=38.0207%2C114.562651; pposn=%E9%87%8D%E5%BA%86%E7%A7%A6%E5%A6%88%E7%81%AB%E9%94%85%EF%BC%88%E8%A3%95%E5%8D%8E%E5%BA%97%EF%BC%89; rvct=76; __utmb=211559370.7.9.1457792016730",
             "Host": "cd.meituan.com",
-            "Referer": "http://cd.meituan.com/category/huoguo/all/page3?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp73gir",
+            "Referer": "http://cd.meituan.com/category/huoguo/all/page{}?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp73gir".format(
+                self.page),
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",
             "Content-Length": "0",
             "X-Requested-With": "XMLHttpRequest"
@@ -49,7 +55,8 @@ class HuoguoSpider(scrapy.Spider):
         my_request.add_header("Host", "cd.meituan.com")
         my_request.add_header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0")
         my_request.add_header("Referer",
-                              "http://cd.meituan.com/category/huoguo/all/page3?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp7aqrm")
+                              "http://cd.meituan.com/category/huoguo/all/page{}?mtt=1.index%2Fdefault%2Fpoi.0.0.ilp7aqrm".format(
+                                  self.page))
         my_request.add_header("X-Requested-With", "XMLHttpRequest")
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
         opener.addheaders.append(('Cookie', headers.get("Cookie")))
@@ -67,3 +74,6 @@ class HuoguoSpider(scrapy.Spider):
             item["count"] = count
             item["addr"] = addr
             yield item
+
+        count = self.page
+        self.page = count + 1
